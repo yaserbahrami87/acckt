cbffb67512895ec5e55c5d7c8fd9c10ed7343249
Yaser Bahrami <yaser.bahrami@gmail.com>
2021-12-22 22:28:53 +0330

    Fix bug responsive

--- resources/views/sarmayeh/index.blade.php
+++ resources/views/sarmayeh/index.blade.php
@@ -664,19 +664,19 @@
                     <div class="col-xl-3 col-lg-3 col-md-3"></div>
                     <div class="col-6">
                         <div class="row">
-                            <div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2" >
+                            <div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-4" >
                                 <img src="../acckt/img/social/aparat.png">
                             </div>
-                            <div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2" >
+                            <div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-4" >
                                 <img src="../acckt/img/social/instagram.png">
                             </div>
-                            <div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2" >
+                            <div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-4" >
                                 <img src="../acckt/img/social/linkedin.png">
                             </div>
-                            <div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2" >
+                            <div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-4" >
                                 <img src="../acckt/img/social/telegram.png">
                             </div>
-                            <div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2" >
+                            <div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-4" >
                                 <img src="../acckt/img/social/twitter.png">
                             </div>
                             <div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2" >
